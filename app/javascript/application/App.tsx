3ebbbb7807d78ad38d8ec8277f2b34d9c1aab0c7
eileen_kang <eileen_kang@tech.gov.sg>
2024-01-07 09:13:35 +0800

    chore: setup query client in App

--- app/javascript/application/App.tsx
+++ app/javascript/application/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Root from './Root';
 
 const AdminRoot = React.lazy(() => import('application/admin'));
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Root />}>
-          <Route path="admin/*" element={<AdminRoot />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route path="admin/*" element={<AdminRoot />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
